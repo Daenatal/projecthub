0caabce61f2eeca06ea3a7f4a5bf784f7bda3823
Jesus <daenatal@yahoo.com>
2023-02-20 20:54:38 -0600

    adding basic home page wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
+import './categories.styles.scss';
+
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hobbies',
+    },
+    {
+      id: 2,
+      title: 'Games',
+    },
+    {
+      id: 3,
+      title: 'Shows',
+    },
+    {
+      id: 4,
+      title: 'Movies',
+    },
+    {
+      id: 5,
+      title: 'Projects',
+    }
+  ]
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Enter Hobby</h2>
-          <p>Select Now</p>
+      {categories.map(({ title, id }) => (
+        <div key={id} className='category-container'>
+          <div className='background-image' />
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>See All</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Enter Hobby</h2>
-          <p>Select Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Enter Hobby</h2>
-          <p>Select Now</p>
-        </div>
-      </div>
+      ))}
+
     </div>
   );
 };
