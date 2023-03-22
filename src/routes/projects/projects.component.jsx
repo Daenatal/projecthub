72506044e6a24a2eac860261e8a49390e9115ce0
Jesus <daenatal@yahoo.com>
2023-03-21 23:00:06 -0500

    adding backend directory, setting up server side data management

--- src/routes/projects/projects.component.jsx
+++ src/routes/projects/projects.component.jsx
@@ -1,6 +1,27 @@
+import { Fragment } from "react";
+
+import './projects.styles.scss';
+
 const Projects = () => {
     return (
-        <h1>I am the projects page.</h1>
+        <Fragment>
+            <section className="animated-grid">
+                <div className="card">a</div>
+                <div className="card">b</div>
+                <div className="card">c</div>
+                <div className="card">d</div>
+                <div className="card">e</div>
+                <div className="card">f</div>
+                <div className="card">g</div>
+                <div className="card">h</div>
+                <div className="card">i</div>
+                <div className="card">j</div>
+                <div className="card">k</div>
+                <div className="card">l</div>
+                <div className="card">main</div>
+            </section>
+        </Fragment>
+        
     )
 }
 
